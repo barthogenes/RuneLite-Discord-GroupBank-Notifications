7410919b9e0cd0242f4c16dc34fcfd32fa5ed038
Thomas Williams <32136795+tawaim@users.noreply.github.com>
2026-03-03 01:31:00 -0500

    fixed package declaration

--- src/main/java/com/discordgroupbanknotifications/DiscordGroupBankNotificationsPlugin.java
+++ src/main/java/com/discordgroupbanknotifications/DiscordGroupBankNotificationsPlugin.java
@@ -1,4 +1,4 @@
-package net.runelite.client.plugins.discordgroupbanknotifications;
+package com.discordgroupbanknotifications;
 
 import com.google.common.base.Strings;
 import com.google.inject.Provides;
